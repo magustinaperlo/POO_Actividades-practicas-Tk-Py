20575413aecce6958147c9a0a38578aa751c5539
Juan Suarez <jp.juansuarez@gmail.com>
2022-09-13 11:44:16 -0300

    Agregado una version 2

--- 1_ContCreciente.py
+++ 1_ContCreciente.py
@@ -1,39 +1,15 @@
 from tkinter import *
+root = Tk()
+root.counter = 0
 
-
-
-class contCreciente(Frame):
-    def __init__(self, master, *x,**y):
-        Frame.__init__(self, master, *x, **y)
-        self.parent = master
-        self.grid()
-        self.createWidgets()
+def clicked():
+    root.counter += 1
+    L['text'] = 'Contador: ' + str(root.counter)
         
-    def contadorCreciente(self, num):
-        num += 1
-        Label(text=num,font=('Consolas 13 bold')).pack()
-    
-    # def sumarButton(self):
-    #     self.num == num
-    #     num += 1
-    #     return num
-        
+b = Button(root, text="+1", command=clicked)
+b.pack()
 
-    def createWidgets(self):
-        self.display = Label(self, font=("Consolas", 18), relief=GROOVE, justify=CENTER, bg='white', fg='blue', borderwidth=2)
-        # self.display.insert(0, "0")
-        self.display.grid(row=0, column=0, columnspan=4, sticky="nsew")
+L = Label(root, text="Contador: 0")
+L.pack()
 
-        self.sumarButton = Button(self, font=("Consolas", 12), fg='red', text="Sumar +1", highlightbackground='red', command=lambda: contadorCreciente("1"))
-        self.sumarButton.grid(row=1, column=0, sticky="nsew")
-
-
-
-    
-
-contadorP = Tk()
-contadorP.title("ContCreciente")
-contadorP.resizable(False, False)
-contadorP.config(cursor="top_left_arrow")
-root = contCreciente(contadorP).grid()
-contadorP.mainloop()
+root.mainloop()
