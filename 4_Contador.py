fa77ce81ae938b39b6c0b2825e4f93dfc9e15b4b
Juan Suarez <jp.juansuarez@gmail.com>
2022-09-14 19:58:51 -0300

    Updated excersice 4

--- 4_Contador.py
+++ 4_Contador.py
@@ -8,6 +8,20 @@ ventana.config(bg="grey")
 campoNum = StringVar()
 campoNum.set(0)
 
+def FcountUp():
+    num=int(campo.get())
+    num += 1
+    campoNum.set(num)
+
+def FcountDown():
+    num=int(campo.get())
+    num -= 1
+    campoNum.set(num)
+
+def Freset():
+    num = 0
+    campoNum.set(num)
+
 #Frame centrado, ubicaremos todos los elementos aqui.
 frameCont = Frame(ventana)
 frameCont.place(relx= 0.5, rely=0.5, anchor=CENTER)
@@ -21,15 +35,15 @@ campo = Entry (frameCont, textvariable=campoNum)
 campo.grid(row=0, column=1)
 
 #Boton 1
-countUp = Button (frameCont, text="Count Up")
+countUp = Button (frameCont, text="Count Up", command=FcountUp)
 countUp.grid(row=0, column=2)
 
 #Boton 2
-countDown = Button (frameCont, text="Count Down")
+countDown = Button (frameCont, text="Count Down", command=FcountDown)
 countDown.grid(row=0, column=3)
 
 #Boton 3
-reset = Button(frameCont, text="Reset")
+reset = Button(frameCont, text="Reset", command=Freset)
 reset.grid(row=0, column=4)
 
 
