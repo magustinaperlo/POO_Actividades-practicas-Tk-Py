98e3a5a6516828de650b6ca49743ef4616c79850
Juan Suarez <jp.juansuarez@gmail.com>
2022-09-14 19:10:02 -0300

    Added exercise 4.

---
+++ 4_Contador.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+ventana = Tk()
+ventana.geometry("600x200")
+ventana.title("Contador")
+ventana.config(bg="grey")
+
+txtvar = StringVar()
+txtvar.set(0)
+
+#Frame centrado, ubicaremos todos los elementos aqui.
+frameCont = Frame(ventana)
+frameCont.place(relx= 0.5, rely=0.5, anchor=CENTER)
+
+#Texto Contador
+textoContador = Label(frameCont, text="Contador")
+textoContador.grid(row=0, column=0)
+
+#Campo de numeros
+campo = Entry (frameCont)
+campo.grid(row=0, column=1)
+
+#Boton 1
+countUp = Button (frameCont, text="Count Up")
+countUp.grid(row=0, column=2)
+
+#Boton 2
+countDown = Button (frameCont, text="Count Down")
+countDown.grid(row=0, column=3)
+
+#Boton 3
+reset = Button(frameCont, text="Reset")
+reset.grid(row=0, column=4)
+
+
+ventana.mainloop()
