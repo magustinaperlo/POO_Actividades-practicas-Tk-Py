142448b0dfa5a1717f0c3e5ff7cfe0e6733819c1
Juan Suarez <jp.juansuarez@gmail.com>
2022-09-14 12:54:06 -0300

    Added basic interface

---
+++ 3_Cont.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+
+
+# def factorial():
+
+#     nfact = n
+#     return nfact
+
+ventana = Tk()
+ventana.geometry("600x200")
+ventana.title("Factorial")
+ventana.config(bg="grey")
+
+
+frameFact = Frame(ventana)
+frameFact.place(bordermode="inside", relx= 0.5, rely=0.5, anchor=CENTER)
+
+etiqueta1 = Label(frameFact, text="n")
+etiqueta1.grid(row=0, column=0)
+
+lineEdit1 = Entry(frameFact,state="readonly")
+lineEdit1.grid(row=0, column=1)
+
+etiqueta2 = Label(frameFact, text="Factorial(n)")
+etiqueta2.grid(row=0, column=2)
+
+lineEdit2 = Entry(frameFact, state="readonly")
+lineEdit2.grid(row=0, column=3)
+
+buttonSiguiente = Button(frameFact, text="Siguiente")
+buttonSiguiente.grid(row=0, column=4)
+
+
+ventana.mainloop()
