3c2bd6287e4bedc6d42c63f9edcbd290901720c1
Juan Suarez <jp.juansuarez@gmail.com>
2022-09-14 19:13:23 -0300

    Changed contador function place

--- 1_ContCreciente.py
+++ 1_ContCreciente.py
@@ -5,6 +5,11 @@ ventana = Tk()
 ventana.geometry("300x200")
 ventana.title("ContCreciente.py")
 
+def contador():
+    num=int(etiquetaCont.get())
+    num += 1
+    txtvar.set(num)
+
 txtvar=StringVar()
 txtvar.set(0)
 
@@ -22,10 +27,7 @@ etiquetaNom.grid(row=0, column=0)
 etiquetaCont = Entry(frameCont, textvariable=txtvar)
 etiquetaCont.grid(row=0, column=1)
 
-def contador():
-    num=int(etiquetaCont.get())
-    num += 1
-    txtvar.set(num)
+
 
 botonCont = Button(frameCont, text="+", command = contador)
 botonCont.grid(row=0, column=2)
